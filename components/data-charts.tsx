5d1cc14a9613b59b9c63e47dc82d7bf9d9ccef14
AaronMonti <montiveroaaron@gmail.com>
2024-02-02 15:52:51 -0300

    hotfix/chatjs_register

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -1,7 +1,26 @@
 'use client'
 // components/DataCharts.tsx
 import { Chart, ChartProps } from 'react-chartjs-2'
-import { Chart as ChartJS } from 'chart.js/auto';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  BarController,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  BarController,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function DataCharts ({ type, data, options, width, height }: ChartProps) {
   return (
